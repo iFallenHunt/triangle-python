19eced61fdbc2969efca8b7a70c16bfbe1e2534f
Lucas Santos <lukas_santos.silva@hotmail.com>
2022-05-25 17:00:29 -0300

    to calculate the base of a triangle

---
+++ triangle.py
@@ -0,0 +1,7 @@
+area = int
+base = int(input('Enter the vase of the trinagle: '))
+height = int(input('Enter the height of the triangle: '))
+
+area = base * height / 2 
+
+print('The area of the triangle is:', area)
